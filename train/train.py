2ebaa2bf6db899f210880b384b32cf03529ed296
carey <980328708@qq.com>
2024-08-07 22:31:51 +0800

    add tools for datasets

---
+++ train/train.py
@@ -0,0 +1,28 @@
+import torch
+from ultralytics import YOLO
+
+def main():
+    # 检查CUDA是否可用
+    if not torch.cuda.is_available():
+        raise SystemError("CUDA not available. Please check your PyTorch installation and GPU configuration.")
+
+    # 加载YOLOv8模型
+    model = YOLO('yolov8n.yaml')  # 加载预训练模型 yolov8n.pt
+
+    # 确定设备/
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # 将模型移动到指定设备
+    model.to(device)
+
+    # 配置训练参数
+    data = 'config-Cursor.yaml'
+    epochs = 500
+
+    # 开始训练并指定设备
+    model.train(data=data, epochs=epochs, device=device, imgsz=1024, batch=5)
+
+
+if __name__ == '__main__':
+
+    main()
+
